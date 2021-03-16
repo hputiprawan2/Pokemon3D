03321e441c3587473e648e7e782c177cbd005e54
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-03-16 00:17:39 -0500

    [Add] Recognized the image card from the camera

--- Pokemon3D/ViewController.swift
+++ Pokemon3D/ViewController.swift
@@ -22,19 +22,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration() // looking for a specific image that we provide
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            // Bundle.main = location of the current file
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 1
+            print("Successfully find the card in the real world, and turn it into ARReferenceImage.")
+        }
         // Run the view's session
         sceneView.session.run(configuration)
     }
